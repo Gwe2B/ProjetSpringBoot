4fa5bcac991a0b591a231c74a0791cd783e2dc70
Gwe2B <gwenael.guiraud@gmail.com>
2022-11-16 14:44:25 +0100

    Creating the my-movies micro-services on port 8080

---
+++ my-movies/src/test/java/com/gwe2b/mymovies/MyMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwe2b.mymovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
